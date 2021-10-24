531e4176c0072e8cf29e7cdabbe8088e886672fd
bystand <92830997+bystanda@users.noreply.github.com>
2021-10-24 20:18:16 +0000

    added actual random thing

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const options = [
+  'random 1',
+  'random 2',
+  'random 3',
+  'random 4',
+  'random 5'
+]
+
 function random() {
-  document.getElementById('text').innerHTML = 'yes'
+  document.getElementById('text').innerHTML = options[getRandomInt(options.length)]
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
